6a2d2ea63ca2c94c169d1d76725ef999c44a0df3
Elhassan7 <chakouki.el.hassan@gmail.com>
2022-04-13 02:32:12 +0000

    git push

--- web Scraping/scrape_url_page.py
+++ web Scraping/scrape_url_page.py
@@ -1,4 +1,5 @@
 from venv import create
+from click import open_file
 import requests
 from bs4 import BeautifulSoup
 
@@ -19,9 +20,22 @@ def scrape_url_page(url):
     pg=requests.get(url, headers=headers)
 
     html_soup=BeautifulSoup(pg.text,'html.parser')
-    url_current=html_soup.find("span",class_="s-pagination-strip").find_all("a",{"href"})
-    print(html_soup)
-    print(url_current)
+    with open("url_pages.txt", "a") as url_pages:
+        for i in html_soup.find("span",class_="s-pagination-strip").find_all("a",href=True):
+            url_pages.write("https://www.amazon.com"+i["href"]+"\n")
+    
+    print(len(html_soup))
 
-url="https://www.amazon.com/s?i=mobile&rh=n%3A2335752011%2Cp_72%3A2491149011&page=12&pd_rd_r=30305d08-93c6-4f70-aca0-5bdcd0619f0a&pd_rd_w=V3rlu&pd_rd_wg=cnq1i&pf_rd_p=f5c158e1-98f7-4998-94b8-d7306c066086&pf_rd_r=Q0RJ0DQSNB5EM8EWZX6G&qid=1649612117&ref=sr_pg_12"
-scrape_url_page(url)
+url="https://www.amazon.com/s?i=mobile&rh=n%3A2335752011%2Cp_72%3A2491149011&page=13&pd_rd_r=30305d08-93c6-4f70-aca0-5bdcd0619f0a&pd_rd_w=V3rlu&pd_rd_wg=cnq1i&pf_rd_p=f5c158e1-98f7-4998-94b8-d7306c066086&pf_rd_r=Q0RJ0DQSNB5EM8EWZX6G&qid=1649811384&ref=sr_pg_12"
+
+
+def in_urls(url):
+    url_pages=open_file(r'D:\INPT2\Data mining\DataMining-Project\url_pages.txt','r')
+    for urls in url_pages:
+        print(urls,"---------")
+        if(url == urls ):
+            return True
+    return False
+
+#scrape_url_page(url)
+print(in_urls(url))
